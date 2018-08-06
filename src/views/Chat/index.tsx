154acbd1d7a5f08e438822f25f0431bdcf883825
Vincent Chu <vincentchu@gmail.com>
2018-08-06 07:40:48 -0700

    Add in ability to create offer

--- src/views/Chat/index.tsx
+++ src/views/Chat/index.tsx
@@ -1,15 +1,36 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
+import { connect, DispatchProp } from 'react-redux'
 
-import { ConnectionState } from '../../state/connection'
+import { ConnectionState, addOffer } from '../../state/connection'
 
 type ChatProps = {
-}
+  connection?: RTCPeerConnection
+  offer?: RTCSessionDescriptionInit
+} & DispatchProp
 
 const Chat: React.SFC<ChatProps> = (props) => {
+  const { offer, connection, dispatch } = props
+  const onClick = () => {
+    console.log('Creating offer')
+
+    connection.createOffer().then((createdOffer) => dispatch(addOffer(createdOffer)))
+  }
+
   return (
     <div>
       <h1>Chat</h1>
+
+      { !offer && <button onClick={onClick}>Start Chat</button> }
+
+      { offer && (
+        <div>
+          <h4>Created Offer</h4>
+          <ul>
+            <li>SDP: { offer.sdp }</li>
+            <li>Type: { offer.type }</li>
+          </ul>
+        </div>
+      ) }
     </div>
   )
 }
@@ -17,7 +38,11 @@ const Chat: React.SFC<ChatProps> = (props) => {
 const mapStateToProps = (state: {
   connection: ConnectionState
 }) => {
+  const { connection, offer } = state.connection
+
   return {
+    connection,
+    offer,
   }
 }
 
