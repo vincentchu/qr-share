b1fa5a75e77fb3167b206dc182ae3edf1794e8a8
Vincent Chu <vincentchu@gmail.com>
2018-08-09 16:23:58 -0700

    Files being displayed and progress

--- src/views/Receive/index.tsx
+++ src/views/Receive/index.tsx
@@ -6,18 +6,45 @@ import { connect, DispatchProp } from 'react-redux'
 import { receiveFiles } from './file-receiver'
 import loadingComponent from '../loading-component'
 import HandshakeApi from '../../handshake-api'
+import { ReceiverState, FileStore, FileStub } from '../../state/receiver'
 
 type RouteProps = {
   id: string
 }
 
 type ReceiveProps = {
+  currentFile?: FileStub
+  currentTransfer?: FileStore
+  files: File[]
 } & RouteComponentProps<RouteProps> & DispatchProp
 
 const Receive: React.SFC<ReceiveProps> = (props) => {
+  const { currentFile, currentTransfer, files } = props
+  const percent = currentTransfer && currentFile && (100.0 * currentTransfer.bytesReceived / currentFile.size)
+
   return (
     <div>
       <h1>Receive Share</h1>
+
+      { currentTransfer && currentFile && (
+        <div>
+          <h3>Downloading { currentFile.name }</h3>
+
+          <p>{ currentTransfer.bytesReceived } / { currentFile.size } ({ percent }%)</p>
+        </div>
+      ) }
+
+      <div>
+        <h3>Files Completed</h3>
+
+        <ul>
+          { files.map((file) => (
+            <li key={file.name}>
+              { file.name }
+            </li>
+          )) }
+        </ul>
+      </div>
     </div>
   )
 }
@@ -34,4 +61,19 @@ const loader = (dispatch: Dispatch, props: ReceiveProps): Promise<any> => {
   return h.receiveHandshake()
 }
 
-export default connect()(loadingComponent(loader, Receive))
+const mapStateToProps = (state: {
+  receiver: ReceiverState
+}) => {
+  const { currentFile, currentTransfer, completedFiles } = state.receiver
+
+  const keys = Object.keys(completedFiles)
+  const files = keys.map((key) => completedFiles[key])
+
+  return {
+    currentFile,
+    currentTransfer,
+    files,
+  }
+}
+
+export default connect(mapStateToProps)(loadingComponent(loader, Receive))
